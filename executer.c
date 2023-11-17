abd19296625e4f53deed158f9582f95f5d948132
Sebo26 <sebomaduna26@gmail.com>
2023-11-17 13:05:08 -0800

    No 1

--- executer.c
+++ executer.c
@@ -8,29 +8,18 @@
  */
 void executer(const char *command)
 {
-	char const **args = malloc(2 * sizeof(char *));
 	pid_t child_ID;
-
-	if (args == NULL)
-	{
-		perror("Memory allocation error");
-		exit(EXIT_FAILURE);
-	}
-	args[0] = command;
-	args[1] = NULL;
 
 	child_ID = fork();
 	if (child_ID == -1)
 	{
 		perror("child_ID not created, fork");
-		free(args);
 		exit(EXIT_FAILURE);
 	}
 	else if (child_ID == 0)
 	{
-		execve(command, (char *const *)args, NULL);
+		execlp(command, command, (char *)NULL);
 		perror("Execution not successful");
-		free(args);
 		exit(EXIT_FAILURE);
 	}
 	else
